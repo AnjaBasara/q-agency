669cbfdbef92e2570b4710ea52c291b453eb7802
Anja Basara <anjabasara@outlook.com>
2022-11-07 20:25:39 +0100

    Removed unused auto-generated methods and code cleanup

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Author;
 use App\Services\SymfonySkeletonService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthorController extends Controller
 {
@@ -25,58 +23,14 @@ class AuthorController extends Controller
         ]);
     }
 
-    public function load(int $page = 1)
+    public function load(int $page = 1): JsonResponse
     {
         return SymfonySkeletonService::getAuthors($page)->json();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     public function show(string $id): View
     {
         return view('pages.author', ['author' => SymfonySkeletonService::getAuthor($id)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Author $author
-     * @return Response
-     */
-    public function edit(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Author $author
-     * @return Response
-     */
-    public function update(Request $request, Author $author)
-    {
-        //
     }
 
     public function destroy(string $id): RedirectResponse
